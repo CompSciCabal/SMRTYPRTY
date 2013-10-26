9fb484f63ad106d5c471845915ad38deeb0a6c60
inaimathi <inaimathi@lambda>
2013-10-26 12:32:04 -0400

    -Added 1.3 homework in scheme

--- sicp/chapter-1.3/inaimathi.scm
+++ sicp/chapter-1.3/inaimathi.scm
@@ -76,3 +76,49 @@
   (filetered-accumulate
    (lambda (a) (= 1 (gcd a n)))
    * id 1 inc (inc n)))
+
+;;; 1.34
+(define (f g) (g 2))
+
+; An evaluation error, I think. (f f) reduces to (f 2), which reduces to (2 2), at which point we get an error unless we've defined "the function '2'", and we can't.
+
+;;; 1.35
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (displayln guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (sqrt x)
+  (fixed-point (lambda (y) (average y (/ x y))) 1.0))
+
+;; (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0) => .6180327868852458
+
+;;; 1.36
+;; Done. The function you're looking for is `displayln`
+
+(fixed-point (lambda (x) (/ (log 1000) (log x))) 1.1)
+
+;; 34 steps
+
+(fixed-point (lambda (x) (average x (/ (log 1000) (log x)))) 1.1)
+
+;; 13 steps.
+
+;; Note that these do produce different outputs. The first one returns 4.555538934848503, while the second returns 4.555536364911781. Granted, this satisfies close-enough?, but still.
+
+;;; 1.37
+
+;;; 1.38
+
+;;; 1.39
