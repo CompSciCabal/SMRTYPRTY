81d9b1f32e6a22e1761cd4969ee79142273aae66
ynasser <yomnanasser@gmail.com>
2013-11-10 22:15:52 -0500

    finishing last week's hw

--- sicp/chapter-2.1/ynasser-2.1.rkt
+++ sicp/chapter-2.1/ynasser-2.1.rkt
@@ -1,4 +1,5 @@
-#lang planet neil/sicp
+;#lang planet neil/sicp
+#lang racket
 
 ;; Exercise 2.1. Define a better version of make-rat that handles both positive
 ;; and negative arguments. Make-rat should normalize the sign so that if the 
@@ -104,8 +105,6 @@
 ;; confusing and I had to look up stuff in external resources.
 
 (define zero (lambda (f) (lambda (x) x)))
-(define (add-1 n)
-  (lambda (f) (lambda (x) (f ((n f) x)))))
 
 ;(add-1 zero)
 ;(lambda (f) (lambda (x) (f ((zero f) x))))
@@ -114,11 +113,21 @@
 
 ;; (add-1 one) =>
 (define two (lambda (f) (lambda (x) (f (f x)))))
+(define three (lambda (f) (lambda (x) (f (f (f x))))))
 
-;; what
+;; The new addition function just composes the two numbers it receives. Let f
+;; be a function of x. The n-th number is f^n(x) and the m-th number is f^m(x).
+;; m + n => f^(m+n)(x) => f^m(f^n(x)) == f^n(f^m(x)).
+
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
+;; building upon add-1:
 (define (new+ a b) 
   (lambda (f) (lambda (x) ((a f) ((b f) x)))))
 
+;; for this problem, I used: 
+;; http://www.billthelizard.com/2010/10/sicp-26-church-numerals.html
 (define (inc n) (+ 1 n))
 
 ;; (((new+ two one) inc) 0) -> 3
@@ -127,8 +136,8 @@
 
 ;; 2.7
 (define (make-interval a b) (cons a b))
-(define upper-bound car)
-(define lower-bound cdr)
+(define upper-bound cdr)
+(define lower-bound car)
 
 (define (add-interval  x y)
   (make-interval (+ (lower-bound x) (lower-bound y))
@@ -161,6 +170,7 @@
 ;; I hope that's what this question was asking ...
 
 ;; Give examples to show this is not true for multiplication and division ...
+;; ... skipped
 
 ;; 2.10
 (define (mul-interval x y)
@@ -180,8 +190,66 @@
 ;; 2.11
 ;; I suspect that Ben Bitdiddle, the expert systems programmer, was probably
 ;; smoking crack with Rob Ford just before he made this "suggestion".
-;; ... skipped
 
+;; This is almost completely wrong.
+(define (mul-interval-crack x y)
+  (define lb lower-bound)
+  (define ub upper-bound)
+  (cond
+    ;; [a,b]*[c,d]
+    ;; [+ +]x[+ +]
+    [(andmap positive? (list (lb x) (ub x) (lb y) (ub y)))
+     (make-interval (* (lb x) (lb y)) (* (ub x) (ub y)))]
+    ;; [+ +]x[+ -] (not possible)
+    ;; [+ +]x[- +] 
+    [(and (negative? (lb y)) (andmap positive? (list (lb x) (ub x) (lb y))))
+     (make-interval (* (lb x) (lb y)) (* (ub x) (ub y)))]
+    ;; [+ +]x[- -] => (ad, bc)
+    [(andmap positive? (list (lb x) (ub x)))
+     (make-interval (* (lb x) (ub y)) (* (ub x) (lb y)))]
+    ;; [- +]x[+ +] normal case
+    ;; [- +]x[- +] the special case ... todo
+    ;; [- +]x[+ -] (not possible)
+    ;; [- +]x[- -] => (bd, ac)
+    [(and (andmap negative? (list (lb x) (lb y) (ub y))) (positive? (ub x)))
+     (make-interval (* (ub x) (ub y)) (* (lb x) (lb y)))]
+    ;; the other two [- +] cases from aboveS
+    [(and (negative? (lb x)) (positive? (ub x)))
+     (make-interval (* (lb x) (lb y)) (* (ub x) (ub y)))]
+    ;; [- -]x[+ +] => (bd,ac)
+    [(and (andmap negative? (list (lb x) (ub x)))
+          (andmap positive? (list (lb y) (ub y))))
+     (make-interval (* (ub x) (ub y)) (* (lb x) (lb y)))]
+    ;; [- -]x[+ -] (not possible)
+    ;; [- -]x[- +] 
+    [(and (andmap negative? (list (lb x) (ub x) (lb y))) (positive? (ub y)))
+     (make-interval (* (ub x) (ub y)) (* (lb x) (lb y)))]
+    ;; [- -]x[- -] 
+    [else (make-interval (* (ub x) (ub y)) (* (lb x) (lb y)))]))
+
+;; Tests for 2.11 stolen from Bill the Lizard:
+;(define a (make-interval 2 4))
+;(define b (make-interval -2 4))
+;(define c (make-interval -4 -2))
+;> (mul-interval a a)
+;(4 . 16)
+;> (mul-interval a b)
+;(-8 . 16)
+;> (mul-interval a c)
+;(-16 . -4)
+;> (mul-interval b a)
+;(-8 . 16)
+;> (mul-interval b b)
+;(-8 . 16)
+;> (mul-interval b c)
+;(-16 . 8)
+;> (mul-interval c a)
+;(-16 . -4)
+;> (mul-interval c b)
+;(-16 . 8)
+;> (mul-interval c c)
+;(4 . 16)
+  
 ;; 2.12
 (define (make-center-percent center pt)
   (make-interval (* (- 1 pt) center)
@@ -191,8 +259,23 @@
   (/ (- (cdr t) (car t)) 20))
 
 ;; 2.13
-;; I have no idea what this is asking.
-;; ... skipped
+;; Assume low percentage tolerances. Show there's a simple formula for the
+;; "approximate percentage tolerance" of the product of two intervals in terms
+;; of the tolerances of the factors. Assume all numbers are positive.
+
+;; In general, (a,b)x(c,d) => (ac,bd).
+;; We want to solve for ?? in this:
+;; (x +- y)*(u +- v) = (xu +- ??)
+;;
+;; The midpoint between ac and bd is (ac + bd)/2. Thus, we could express
+;; (a,b)x(c,d)'s solution in the center/pt form as 
+;; (ac + bd)/2 +- (bd - ac)/2.
+;;
+;; We must work backwards to find the values of x,y,u, and v in this form:
+;; (x +- y)*(u +- v) = (xu +- ??)
+;;
+;; ... not interested in finishing this problem.
+
 
 ;; 2.14
 (define (par1 r1 r2)
@@ -204,16 +287,59 @@
     (div-interval one
                   (add-interval (div-interval one r1)
                                 (div-interval one r2)))))
-;; where did my tests go?
+
+(define a (make-interval 300 2))
+(define b (make-interval 100 3))
+(define ab (div-interval a b))
+(define aa (div-interval a a))
+
+;> ab
+;'(0.02 . 300.3333333333333)
+;> aa
+;'(0.006666666666666667 . 300.5)
 
 ;; 2.15
+
 ;; I suspect she is right, because the less intervals one
 ;; introduces, the less error there is to propagate. I
 ;; haven't a proof, though, just this intuition based
 ;; on many, many physics labs.
 
 
-;; 2.16 ~~ impossible?
+;; 2.16 - Attempt #2
+;;
+;; This is impossible.
+;;
+;; The first solution which came to my mind was kind of science fiction-y
+;; and involved transforming all algebraic expressions (expressed as functions)
+;; into some "normal" form via symbolic computing. This is already a very sketchy
+;; solution because of the giant number of ways an algebraic expression could be
+;; implemented. Then, all expressions would be evaluated after being converted 
+;; to some "normal" form. But after you computed a value and wanted to use it
+;; in another expression, that value would be "impure", because it came from
+;; some other expression and by giving it to another function to use in a 
+;; computation, you'd be contaminating it. Then you'd have to keep track of where
+;; which numbers came from and blah. This particular problem was pointed out by
+;; Dan, Andrey, and Leo at the November 10th meeting. Clearly, this is a ridiculous
+;; solution.
+;;
+;; Above, I wasn't really thinking about interval arithmatic in particular. There
+;; isn't a concept of an "indentity" (as demonstrated by exercise 2.14) the system
+;; we've developed so far. Wikipedia says this about the dependency problem (which
+;; is what this is): 
+;;
+;; "In general, it can be shown that the exact range of values can be achieved,
+;; if each variable appears only once and if f is continuous inside the box. 
+;; However, not every function can be rewritten this way."
+;;
+;; So, as problem 2.15 demonstrated, being able to reduce the number of variables
+;; (especially down to once) is really powerful. Since not every function can be
+;; written this way, then it is impossible.
+
+
+;; original incoherent text is below and a copy should be sent to
+;; http://shitmystudentswrite.tumblr.com/
+
 ;; off-topic: what is the "recursive poisoning clause"?
 ;; i shall ramble now, but there are no proofs
 ;; i don't know if this makes any senses
