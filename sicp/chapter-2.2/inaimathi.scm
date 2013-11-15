1992f0caa92ceffee8350f3e6cc364bd76bfc50b
inaimathi <inaimathi@lambda>
2013-11-15 17:52:41 -0500

    -Addded rest of exercises (hope I didn't leave in too many swears)

--- sicp/chapter-2.2/inaimathi.scm
+++ sicp/chapter-2.2/inaimathi.scm
@@ -78,3 +78,163 @@
       #t
       (begin (fn (car lst))
              (for-each fn (cdr lst)))))
+
+;;; 2.24
+
+;; (1 (2 (3 4)))
+
+;; [ | ] -> [ | ] -> NIL
+;;  |        |
+;;  1       [ | ] -> [ | ] -> NIL
+;;           |        |
+;;           2       [ | ] -> [ | ] -> NIL
+;;                    |        |
+;;                    3        4
+
+;; (1 (2 (3 4)))
+;;  /	\
+;; 1        (2 (3 4))
+;;           /   \
+;;          2    (3 4)
+;;                / \
+;;               3   4
+
+;;; 2.25
+
+;; > (car (cdr (car (cdr (cdr '(1 2 (5 7) 9))))))
+;; 7
+
+;; > (car (car '((7))))
+;; 7
+
+;; > (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr '(1 (2 (3 (4 (5 (6 7))))))))))))))))))
+;; 7
+
+;;; 2.26
+
+;; > (append x y)
+;; (1 2 3 4 5 6)
+
+;; > (cons x y)
+;; ((1 2 3) 4 5 6)
+
+;; > (list x y)
+;; ((1 2 3) (4 5 6))
+
+;;; 2.27
+(define (deep-reverse a-tree)
+  (define (rec tree acc)
+    (cond ((null? tree) acc)
+          ((pair? (car tree))
+           (rec (cdr tree) (cons (rec (car tree) '()) acc)))
+          (else
+           (rec (cdr tree) (cons (car tree) acc)))))
+  (rec a-tree '()))
+
+;;; 2.28
+(define (fringe tree)
+  (cond ((null? tree) '())
+        ((pair? (car tree))
+         (append (fringe (car tree)) (fringe (cdr tree))))
+        (else
+         (cons (car tree) (fringe (cdr tree))))))
+
+;;; 2.29
+
+;; a)
+(define (left-branch mobile)
+  (car mobile))
+
+(define (right-branch mobile)
+  (car (cdr mobile)))
+
+(define (branch-length branch)
+  (car branch))
+
+(define (branch-structure branch)
+  (car (cdr branch)))
+
+;; b)
+(define (branch-weight branch)
+  (total-weight (branch-structure branch)))
+
+(define (total-weight structure)
+  (if (number? structure)
+      structure
+      (+ (branch-weight (left-branch structure))
+         (branch-weight (right-branch structure)))))
+
+;; c)
+(define (torque branch)
+  (* (branch-length branch) (branch-weight branch)))
+
+(define (balanced? structure)
+  (let ((l (left-branch structure))
+        (r (right-branch structure)))
+    (if (number? structure)
+        #t
+        (and (= (torque l) (torque r))
+             (balanced? (branch-structure l))
+             (balanced? (branch-structure r))))))
+
+;; d)
+;; It depends on how honest you were about not reaching beneath the selector abstraction.
+;; If you were, you'll need to change the basic selectors.
+;; If you weren't, you'll need to change any function that resorted directly to representation-specific selectors.
+;; If you were asking about me specifically, I was honest so I won't have to change much.
+
+;;; 2.30
+(define (square-tree tree)
+  (cond ((null? tree) '())
+        ((atom? tree) (square tree))
+        (else
+         (cons (square-tree (car tree))
+               (square-tree (cdr tree))))))
+
+(define (square-tree-too tree)
+  (map (lambda (t) ;;godDAMN it feels good to do that. I think I'll do it again;
+                   ;; (lambda (t)
+                   ;;   (print t)
+                   ;;   (print "That variable was named 't'")
+                   ;;   (print "U Jelly, Common Lisp?"))
+         (if (pair? t)
+             (square-tree t)
+             (square t)))
+       tree))
+
+;;; 2.31
+(define (tree-map fn tree)
+  (map (lambda (t)
+         (if (pair? t)
+             (tree-map fn t)
+             (fn t)))
+       tree))
+
+;;; 2.32
+;; The template they hand us is
+
+;; (define (subsets s)
+;;   (if (null? s)
+;;       (list nil)
+;;       (let ((rest (subsets (cdr s))))
+;;         (append rest (map ?? rest)))))
+
+;; Which I have to mildly modify for Chicken
+
+;; (define (subsets s)
+;;   (if (null? s)
+;;       (list '()) ;; <- that right there
+;;       (let ((rest (subsets (cdr s))))
+;;         (append rest (map ?? rest)))))
+
+(define (subsets s)
+  (if (null? s)
+      (list '())
+      (let ((rest (subsets (cdr s))))
+        (append rest
+                (map (lambda (elem)
+                       (cons (car s) elem))
+                     rest)))))
+
+;;   You want to append all the subsets of s not containing (car s)
+;; to the set of all subsets that do include (car s)
