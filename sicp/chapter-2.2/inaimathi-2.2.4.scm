a42d8cc9eab22837a16ee89438e2d97305565066
inaimathi <inaimathi@lambda>
2013-11-28 22:50:14 -0500

    -Second kick at 2.2.4

--- sicp/chapter-2.2/inaimathi-2.2.4.scm
+++ sicp/chapter-2.2/inaimathi-2.2.4.scm
@@ -170,3 +170,85 @@
 
 ;d)
 ;; .... no. Ok, ok, not right now at least.
+
+;;; Givens Again: The Givensing
+(define (transform-painter painter origin corner1 corner2)
+  (lambda (frame)
+    (let ((m (frame-coord-map frame)))
+      (let ((new-origin (m origin)))
+        (painter (make-frame
+                  new-origin
+                  (sub-vect (m corner1) new-origin)
+                  (sub-vect (m corner2) new-origin)))))))
+
+(define (flip-vert painter)
+  (transform-painter painter
+                     (make-vector 0.0 1.0)
+                     (make-vector 1.0 1.0)
+                     (make-vector 0.0 0.0)))
+
+(define (shrink-to-upper-right painter)
+  (transform-painter painter
+                     (make-vector 0.5 0.5)
+                     (make-vector 1.0 0.5)
+                     (make-vector 0.5 1.0)))
+
+(define (rotate90 painter)
+  (transform-painter painter
+                     (make-vector 1.0 0.0)
+                     (make-vector 1.0 1.0)
+                     (make-vector 0.0 0.0)))
+
+(define (squash-inwards painter)
+  (transform-painter painter
+                     (make-vector 0.0 0.0)
+                     (make-vector 0.65 0.35)
+                     (make-vector 0.35 0.65)))
+
+(define (beside painter1 painter2)
+  (let ((split-point (make-vector 0.5 0.0)))
+    (let ((paint-left (transform-painter painter1
+                                         (make-vector 0.0 0.0)
+                                         split-point
+                                         (make-vector 0.0 1.0)))
+          (paint-right (transform-painter painter2
+                                          split-point
+                                          (make-vector 1.0 0.0)
+                                          (make-vector 0.5 1.0))))
+      (lambda (frame) (paint-left frame) (paint-right frame)))))
+
+;;; 2.50
+(define (flip-vert painter)
+  (transform-painter painter
+                     (make-vector 1.0 0.0)
+                     (make-vector 0.0 0.0)
+                     (make-vector 1.0 1.0)))
+
+(define (rotate180 painter)
+  (transform-painter painter
+                     (make-vector 1.0 1.0)
+                     (make-vector 0.0 1.0)
+                     (make-vector 1.0 0.0)))
+(define (rotate270 painter)
+  (transform-painter painter
+                     (make-painter 0.0 1.0)
+                     (make-painter 0.0 0.0)
+                     (make-painter 1.0 1.0)))
+
+;;; 2.51
+(define (below painter1 painter2)
+  (let ((split-point (make-vector 0.0 0.5)))
+    (let ((paint-top (transform-painter painter2
+                                           split-point
+                                           (make-vector 1.0 0.5)
+                                           (make-vector 0.0 1.0)))
+          (paint-bottom (transform-painter painter1
+                                         (make-vector 0.0 0.0)
+                                         (make-vector 1.0 0.0)
+                                         split-point)))
+      (lambda (frame) (paint-left frame) (paint-right frame)))))
+
+(define (below painter1 painter2)
+  (rotate90 (beside painter1 painter2)))
+
+;;; 2.52
