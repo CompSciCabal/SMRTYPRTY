1659aa8bb2d4a0ffc0d973f07b129787223d0bb5
Andrey Paramonov <github@ndpar.com>
2013-12-01 09:39:27 -0500

    Chapter 2.3.2: theory

--- sicp/chapter-2.3/ndpar-2.3.scm
+++ sicp/chapter-2.3/ndpar-2.3.scm
@@ -31,3 +31,59 @@
 ;; Double quotation
 (eq? (car ''abracadabra)
      (car (quote (quote abracadabra))))
+
+;; -------------------------------------------------------------------
+;; Symbolic Differentiation, p.147
+;; -------------------------------------------------------------------
+
+(define (deriv sexp var)
+  (cond ((number? sexp) 0)
+        ((variable? sexp)
+         (if (same-variable? sexp var) 1 0))
+        ((sum? sexp)
+         (make-sum (deriv (addend sexp) var)
+                   (deriv (augend sexp) var)))
+        ((product? sexp)
+         (make-sum (make-product (multiplier sexp)
+                                 (deriv (multiplicand sexp) var))
+                   (make-product (deriv (multiplier sexp) var)
+                                 (multiplicand sexp))))
+        (else (error "unknown s-expression type -- DERIV" sexp))))
+
+(define variable? symbol?)
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+
+(define addend cadr)
+(define augend caddr)
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
+
+(define multiplier cadr)
+(define multiplicand caddr)
+
+(define (=number? sexp num)
+  (and (number? sexp) (= sexp num)))
+
+;; Tests
+(eq? 1 (deriv '(+ x 3) 'x))
+
+(eq? 'y (deriv '(* x y) 'x))
