42f4054606b264f7a8b2c9de4edfcf7a4ec3299a
ynasser <ynasser@uwaterloo.ca>
2013-12-10 15:36:00 -0500

    a strange pseudo-fractal involving ferns and triangles pretending to be turtles ...

---
+++ experiments/ynasser/fractals/turtles-and-ferns.rkt
@@ -0,0 +1,21 @@
+#lang racket
+;; this requires some patience
+
+(require graphics/turtles)
+(require graphics/turtle-examples)
+
+(turtles)
+(turn 8)
+(turn 80)
+(split (turn/radians (/ pi 2)))
+(split (turn/radians (/ pi 2)))
+(split (turn/radians (/ pi 2)))
+(split (turn/radians (/ pi 2)))
+(split (turn/radians (/ pi 2.5)))
+(split (turn/radians (/ pi 2.5)))
+(split (turn/radians (/ pi 2.5)))
+(split (turn/radians (/ pi 2.5)))
+(split (turn/radians (/ pi 2.5)))
+(draw 150)
+
+(fern1 20)
