89d0b1bd70449bd8cb09398c871bad3e5dfe1030
Ian Garmaise <ian.g@phorixsol.com>
2013-12-13 18:33:24 -0500

    first problem only

---
+++ sicp/chapter-2.3/ian-2.3.2.rkt
@@ -0,0 +1,18 @@
+#lang racket
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
