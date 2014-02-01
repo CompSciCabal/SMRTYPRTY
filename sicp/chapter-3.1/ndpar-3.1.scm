f24e0bc9d41fa5475ffda73c1bf6ff86d9265f8a
Andrey Paramonov <github@ndpar.com>
2014-02-01 11:34:24 -0500

    Exercises 3.1, 3.2

--- sicp/chapter-3.1/ndpar-3.1.scm
+++ sicp/chapter-3.1/ndpar-3.1.scm
@@ -25,3 +25,31 @@
 (= 30 ((acc 'withdraw) 70))
 (= 80 ((acc 'deposit) 50))
 (= 10 ((acc 'withdraw) 70))
+
+;; Exercise 3.1, p.224
+
+(define ((make-accumulator acc) n)
+  (set! acc (+ acc n))
+  acc)
+
+(define A (make-accumulator 5))
+(= 15 (A 10))
+(= 25 (A 10))
+
+;; Exercise 3.2, p.224
+
+(define (make-monitored f)
+  (let ((count 0))
+    (lambda (param)
+      (cond ((eq? param 'how-many-calls?) count)
+            ((eq? param 'reset-count)
+             (set! count 0) count)
+            (else
+             (set! count (+ 1 count))
+             (f param))))))
+
+(define S (make-monitored sqrt))
+(= 5 (S 25))
+(= 1 (S 'how-many-calls?))
+(= 0 (S 'reset-count))
+(= 0 (S 'how-many-calls?))
