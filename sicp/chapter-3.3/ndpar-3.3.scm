2e460843b1d57f39be7abc7d12e5244ec2e2ee03
Andrey Paramonov <github@ndpar.com>
2014-02-19 22:53:06 -0500

    Chapter 3.3.2

--- sicp/chapter-3.3/ndpar-3.3.scm
+++ sicp/chapter-3.3/ndpar-3.3.scm
@@ -85,3 +85,187 @@
 (not (floyd-cycle? '(1 2)))
 (not (floyd-cycle? '(1 2 3)))
 (floyd-cycle? cycle)
+
+;; -------------------------------------------------------------------
+;; Representing Queues, p.261
+;; -------------------------------------------------------------------
+
+(define front-ptr car)
+(define rear-ptr cdr)
+(define set-front-ptr! set-car!)
+(define set-rear-ptr! set-cdr!)
+
+(define (make-queue) (cons nil nil))
+(define (empty-queue? queue) (null? (front-ptr queue)))
+
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an empty queue" queue)
+      (car (front-ptr queue))))
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item nil)))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+          (else
+           (set-cdr! (rear-ptr queue) new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue))))
+
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "DELETE! called with an empty queue" queue))
+        (else
+         (set-front-ptr! queue (cdr (front-ptr queue)))
+         queue)))
+
+;; Exercise 3.21, p.265
+
+(define (print-queue queue)
+  (display (front-ptr queue))
+  (newline))
+
+(define q1 (make-queue))
+(print-queue (insert-queue! q1 'a))
+(print-queue (insert-queue! q1 'b))
+(print-queue (delete-queue! q1))
+(print-queue (delete-queue! q1))
+
+;; Exercise 3.22, p.266
+
+(define (make-q)
+  (let ((front-ptr nil)
+        (rear-ptr nil))
+    (define (empty-q?) (null? front-ptr))
+    (define (insert-q item)
+      (let ((q (cons item nil)))
+        (cond ((empty-q?)
+               (set! front-ptr q)
+               (set! rear-ptr q)
+               front-ptr)
+              (else
+               (set-cdr! rear-ptr q)
+               (set! rear-ptr q)
+               front-ptr))))
+    (define (delete-q)
+      (cond ((empty-q?)
+             (error "DELETE! called with an empty queue" front-ptr))
+            (else
+             (set! front-ptr (cdr front-ptr))
+             front-ptr)))
+    (define (dispatch m)
+      (cond ((eq? m 'insert-queue) insert-q)
+            ((eq? m 'delete-queue) delete-q)))
+    dispatch))
+
+(define q2 (make-q))
+((q2 'insert-queue) 'a)
+((q2 'insert-queue) 'b)
+((q2 'delete-queue))
+((q2 'delete-queue))
+
+;; Exercise 3.23, p.266
+
+(define (cell item)
+  (cons item (cons nil nil)))
+
+(define (link! left-cell right-cell)
+  (set-car! (cdr left-cell) right-cell)
+  (set-cdr! (cdr right-cell) left-cell)
+  left-cell)
+
+;       ┌─┬─┐
+; Deque │*│*┼────┐
+;       └┼┴─┘    │
+;        │       │
+;       ┌┴┐     ┌┴┐
+;     ┌─┤a│  ┌──┤b│ Cells
+;     │ ├─┤  │  ├─┤
+;     │ │*│  │  │*│
+;     │ └┼┘  │  └┼┘
+;     │ ┌┴┐  │  ┌┴┐
+;     │ │*┼──┘  │ │
+;     │ ├─┤     ├─┤
+;     │ │ │    ┌┼*│
+;     │ └─┘    │└─┘
+;     └────────┘
+
+(define (make-deque) (cons nil nil))
+
+(define (empty-deque? d) (null? (car d)))
+
+(define (front-deque d)
+  (if (empty-deque? d)
+      (error "FRONT called with an empty deque" d)
+      (caar d)))
+
+(define (rear-deque d)
+  (if (empty-deque? d)
+      (error "REAR called with an empty deque" d)
+      (cadr d)))
+
+(define (front-insert-deque! d item)
+  (let ((n (cell item)))
+    (cond ((empty-deque? d)
+           (set-car! d n)
+           (set-cdr! d n)
+           d)
+          (else
+           (link! n (car d))
+           (set-car! d n)
+           d))))
+
+(define (rear-insert-deque! d item)
+  (let ((n (cell item)))
+    (cond ((empty-deque? d)
+           (set-car! d n)
+           (set-cdr! d n)
+           d)
+          (else
+           (link! (cdr d) n)
+           (set-cdr! d n)
+           d))))
+
+(define (front-delete-deque! d)
+  (cond ((empty-deque? d)
+         (error "FRONT-DELETE! called with an empty deque" d))
+        (else
+         (set-car! d (cadar d))
+         (set-cdr! (cdar d) nil)
+         d)))
+
+(define (rear-delete-deque! d)
+  (cond ((empty-deque? d)
+         (error "REAR-DELETE! called with an empty deque" d))
+        (else
+         (set-cdr! d (cdddr d))
+         (set-car! (cddr d) nil)
+         d)))
+
+(define (to-list d)
+  (define (iter acc cells)
+    (if (null? cells)
+        (reverse acc)
+        (iter (cons (car cells) acc) (cadr cells))))
+  (iter nil (car d)))
+
+;; Tests
+
+(define d (make-deque))
+(to-list (front-insert-deque! d 5))
+(front-deque d)
+(rear-deque d)
+(to-list (front-insert-deque! d 4))
+(front-deque d)
+(rear-deque d)
+(to-list (rear-insert-deque! d 6))
+(front-deque d)
+(rear-deque d)
+(to-list (front-delete-deque! d))
+(front-deque d)
+(rear-deque d)
+(to-list (rear-delete-deque! d))
+(front-deque d)
+(rear-deque d)
