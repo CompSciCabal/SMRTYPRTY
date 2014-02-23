10833865dbb522ea6e57024a9e302f7b13da06c8
Andrey Paramonov <github@ndpar.com>
2014-02-23 16:20:56 -0500

    Chapter 3.3.3

--- sicp/chapter-3.3/ndpar-3.3.scm
+++ sicp/chapter-3.3/ndpar-3.3.scm
@@ -269,3 +269,51 @@
 (to-list (rear-delete-deque! d))
 (front-deque d)
 (rear-deque d)
+
+;; -------------------------------------------------------------------
+;; Representing Tables, p.270
+;; -------------------------------------------------------------------
+
+(define (assoc key records)
+  (cond ((null? records) false)
+        ((equal? key (caar records)) (car records))
+        (else (assoc key (cdr records)))))
+
+(define (make-table)
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (cdr record)
+                  false))
+            false)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1
+                                  (cons key-2 value))
+                            (cdr local-table)))))
+      'ok)
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation -- TABLE" m))))
+    dispatch))
+
+(define operation-table (make-table))
+(define get (operation-table 'lookup-proc))
+(define put (operation-table 'insert-proc!))
+
+;; Tests
+
+(put 'a 'b 5)
+(get 'a 'b)
