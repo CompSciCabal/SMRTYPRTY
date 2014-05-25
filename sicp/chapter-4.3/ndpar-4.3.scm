38b34d9bd93c441270721b83e3bf3ee9f021603f
Andrey Paramonov <github@ndpar.com>
2014-05-25 13:00:01 -0400

    Parsing natural language

--- sicp/chapter-4.3/ndpar-4.3.scm
+++ sicp/chapter-4.3/ndpar-4.3.scm
@@ -235,3 +235,61 @@
 ;= (1 7 4 6 8 2 5 3)
 ;= (1 7 5 8 2 4 6 3)
 ;...
+
+;; -------------------------------------------------------
+;; Parsing Natural Language, p.420
+;; -------------------------------------------------------
+
+(define nouns '(noun student professor cat class))
+(define verbs '(verb studies lectures eats sleeps))
+(define articles '(article the a))
+(define prepositions '(prep for to in by with))
+
+(define (parse-sentence)
+  (list 'sentence
+        (parse-noun-phrase)
+        (parse-verb-phrase)))
+
+(define (parse-verb-phrase)
+  (define (maybe-extend verb-phrase)
+    (amb verb-phrase
+         (maybe-extend (list 'verb-phrase
+                             verb-phrase
+                             (parse-prepositional-phrase)))))
+  (maybe-extend (parse-word verbs)))
+
+(define (parse-noun-phrase)
+  (define (maybe-extend noun-phrase)
+    (amb noun-phrase
+         (maybe-extend (list 'noun-phrase
+                             noun-phrase
+                             (parse-prepositional-phrase)))))
+  (maybe-extend (parse-simple-noun-phrase)))
+
+(define (parse-simple-noun-phrase)
+  (list 'simple-noun-phrase
+        (parse-word articles)
+        (parse-word nouns)))
+
+(define (parse-prepositional-phrase)
+  (list 'prep-phrase
+        (parse-word prepositions)
+        (parse-noun-phrase)))
+
+(define *unparsed* nil)
+
+(define (parse-word words)
+  (require (not (null? *unparsed*)))
+  (require (memq (car *unparsed*) (cdr words)))
+  (let ((found (car *unparsed*)))
+    (set! *unparsed* (cdr *unparsed*))
+    (list (car words) found)))
+
+(define (parse input)
+  (set! *unparsed* input)
+  (let ((sent (parse-sentence)))
+    (require (null? *unparsed*))
+    sent))
+
+; (parse '(the cat eats))
+; (parse '(the professor lectures to the student with the cat))
