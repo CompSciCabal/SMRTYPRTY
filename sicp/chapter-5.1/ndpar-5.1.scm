db6012039fbfebc142dd8fb6e5bdc5ff16d73037
Andrey Paramonov <github@ndpar.com>
2014-09-13 10:37:23 -0400

    Exercise 5.4

--- sicp/chapter-5.1/ndpar-5.1.scm
+++ sicp/chapter-5.1/ndpar-5.1.scm
@@ -49,3 +49,39 @@
    (goto (label sqrt-iter))
  done
    (perform (op print) (reg g)))
+
+;; Exercise 5.4, p.510
+
+; a. Recursive exponentiation
+(controller
+   (assign continue (label done))
+ expt-loop
+   (test (op =) (reg n) (const 0))
+   (branch (label base-case))
+   (save continue)
+   (assign n (op -) (reg n) (const 1))
+   (assign continue (label after-expt))
+   (goto (label expt-loop))
+ after-expt
+   (restore continue)
+   (assign val (op *) (reg b) (reg val))
+   (goto (reg continue))
+ base-case
+   (assign val (const 1))
+   (goto (reg continue))
+ done)
+
+; b. Iterative exponentiation
+(controller
+   (assign b (op read))
+   (assign n (op read))
+   (assign c (reg n))
+   (assign p (const 1))
+ expt-iter
+   (test (op =) (reg c) (const 0))
+   (branch (label done))
+   (assign c (op -) (reg c) (const 1))
+   (assign p (op *) (reg b) (reg p))
+   (goto (label expt-iter))
+ done
+   (perform (op print) (reg p)))
