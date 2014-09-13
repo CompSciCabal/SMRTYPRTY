4270b6f55fc36b56a83ccb5571b54992098062d5
Andrey Paramonov <github@ndpar.com>
2014-09-13 11:40:24 -0400

    Exercise 5.6 in assembly language

--- sicp/chapter-5.1/ndpar-5.1.scm
+++ sicp/chapter-5.1/ndpar-5.1.scm
@@ -112,3 +112,40 @@
    (goto (label expt-iter))
  done
    (perform (op print) (reg p)))
+
+;; Exercise 5.6, p.512
+;; Run in simulator
+
+(define fib-machine
+  (make-machine
+    '(n val)
+    (list (list '< <) (list '- -) (list '+ +))
+    '((assign continue (label fib-done))
+      fib-loop
+      (test (op <) (reg n) (const 2))
+      (branch (label immediate-answer))
+      (save continue)
+      (assign continue (label afterfib-n-1))
+      (save n)
+      (assign n (op -) (reg n) (const 1))
+      (goto (label fib-loop))
+      afterfib-n-1
+      (restore n)
+      (assign n (op -) (reg n) (const 2))
+      (assign continue (label afterfib-n-2))
+      (save val)
+      (goto (label fib-loop))
+      afterfib-n-2
+      (assign n (reg val))
+      (restore val)
+      (restore continue)
+      (assign val (op +) (reg val) (reg n))
+      (goto (reg continue))
+      immediate-answer
+      (assign val (reg n))
+      (goto (reg continue))
+      fib-done)))
+
+(set-register-contents! fib-machine 'n 6)
+(start fib-machine)
+(get-register-contents fib-machine 'val) ;=> 8
