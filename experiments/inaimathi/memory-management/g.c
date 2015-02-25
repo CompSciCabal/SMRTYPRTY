896480c00a9821de0249645733a379ecf306ca8e
inaimathi <inaimathi@self>
2015-02-24 22:10:01 -0500

    -Simple demonstration of basic mark/sweep

--- experiments/inaimathi/memory-management/g.c
+++ experiments/inaimathi/memory-management/g.c
@@ -6,6 +6,7 @@ enum LispType { LISP_INTEGER, LISP_CHARACTER, LISP_PRIMITIVE, LISP_CONS, LISP_FR
 struct Memory; // need it for the *primitive pointer type
 struct Cell {
   enum LispType type;
+  int mark;
   union {
     int integer;
     char character;
@@ -17,11 +18,13 @@ struct Cell {
 
 ///// Memory
 struct Memory {
-  int memory_size;
+  int size;
   struct Cell* all;
   struct Cell* free;
 
   struct Cell* nil;
+
+  struct Cell* root;
 };
 
 struct Memory* new_memory(int cell_count) {
@@ -30,40 +33,66 @@ struct Memory* new_memory(int cell_count) {
   struct Cell* nil = malloc(sizeof(struct Cell));
   struct Cell* all_memory = malloc(sizeof(struct Cell) * cell_count);
 
-  // Initialize the CDR pointers
+  // Initialize cons pointers
   struct Cell* cur;
   int i;
   cur = &all_memory[0];
   cur->type = LISP_FREE;
+  cur->car.cell = nil;
   cur->cdr = nil; // The "last" one links to NIL
   for (i = 1; i < cell_count; ++i) {
     cur = &all_memory[i];
     cur->type = LISP_FREE;
+    cur->car.cell = nil;
     cur->cdr = &all_memory[i-1];
   };
-  
-  mem->memory_size = cell_count;
+
+  mem->size = cell_count;
   mem->free = cur;
   mem->nil = nil;
   mem->all = all_memory;
   return mem;
 }
 
-void print_memory(struct Memory* mem) {
-  int i;
-  struct Cell* cur;
-  for (i = 0; i < mem->memory_size; ++i) {
-    cur = &mem->all[i];
-    if (cur->type == LISP_FREE) printf(".");
-    else printf("|");
-  }
-  printf("\n");
+void deallocate(struct Memory* mem, struct Cell* cell) {
+  cell->type = LISP_FREE;
+  cell->cdr = mem->free;
+  mem->free = cell;
 }
 
 struct Cell* allocate(struct Memory* mem) {
-  struct Cell* new = mem->free;
-  mem->free = mem->free->cdr;
-  return new;
+  struct Cell* new;
+  if (mem->nil == mem->free) {
+    printf("OUT OF MEMORY\n");
+    exit(1);
+  } else {
+    new = mem->free;
+    mem->free = mem->free->cdr;
+    return new;
+  }
+}
+
+void mark(struct Memory* mem, struct Cell* cell) {
+  if ((cell != mem->nil) && (cell->mark == 0)) {
+    cell->mark = 1;
+    if (cell->type == LISP_CONS) {
+      mark(mem, cell->car.cell);
+      mark(mem, cell->cdr);
+    }
+  }
+}
+
+void sweep(struct Memory* mem) {
+  int i;
+  struct Cell* cur;
+  for (i = 0; i < mem->size; ++i) {
+    cur = &mem->all[i];
+    if (cur->mark) {
+      cur->mark = 0;
+    } else {
+      deallocate(mem, cur);
+    }
+  }
 }
 
 ///// Allocating primitives
@@ -95,10 +124,65 @@ struct Cell* new_cons(struct Memory* mem, struct Cell* a, struct Cell* b) {
   new->cdr = b;
 }
 
+///// Testing/demo functions
+void print_memory(struct Memory* mem) {
+  int i;
+  struct Cell* cur;
+  for (i = 0; i < mem->size; ++i) {
+    cur = &mem->all[i];
+    switch (cur->type) {
+    case LISP_FREE: printf("."); break;
+    case LISP_INTEGER: printf("i"); break;
+    case LISP_CONS: printf("|"); break;
+    case LISP_CHARACTER: printf("c"); break;
+    default: printf("o");
+    }
+    if (cur->mark) printf("'");
+    else printf(" ");
+  }
+  printf("\n");
+}
+
+void fill_memory(struct Memory* mem) {
+  while (mem->nil != mem->free) {
+    new_integer(mem, 34);
+  }
+}
+
+
 ///// Main thing
 int main() {
-  struct Memory* mem = new_memory(100);
-  printf("Testing\n");
+  struct Cell* c;
+  struct Cell* lst;
+  struct Memory* mem = new_memory(60);
+
   print_memory(mem);
+
+  c = new_cons(mem, new_character(mem, 'c'), new_integer(mem, 34));
+  print_memory(mem);
+  
+  lst = new_cons(mem, new_integer(mem, 1), new_cons(mem, new_integer(mem, 2), new_cons(mem, new_integer(mem, 3), mem->nil)));
+  mem->root = lst;
+  print_memory(mem);
+
+  fill_memory(mem);
+  print_memory(mem);
+
+  deallocate(mem, c);
+  print_memory(mem);
+
+  mark(mem, mem->root);
+  print_memory(mem);
+  sweep(mem);
+  print_memory(mem);  
+
+  lst = new_cons(mem, new_character(mem, 'a'), new_cons(mem, new_character(mem, 'b'), new_cons(mem, new_character(mem, 'c'), mem->nil)));
+  mem->root = lst;
+  print_memory(mem);
+  mark(mem, mem->root);
+  print_memory(mem);
+  sweep(mem);
+  print_memory(mem);
+
   return 0;
 }
