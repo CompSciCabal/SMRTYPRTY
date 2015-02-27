e455b34ee2d6409c2f48e4ce9366bb8da21d0b5a
inaimathi <inaimathi@self>
2015-02-27 17:20:05 -0500

    -Fixed bug in deallocate

--- experiments/inaimathi/memory-management/g.c
+++ experiments/inaimathi/memory-management/g.c
@@ -21,7 +21,6 @@ struct Memory {
   int size;
   struct Cell* all;
   struct Cell* free;
-
   struct Cell* nil;
 
   struct Cell* root;
@@ -50,26 +49,16 @@ struct Memory* new_memory(int cell_count) {
   mem->size = cell_count;
   mem->free = cur;
   mem->nil = nil;
+  mem->root = nil;
   mem->all = all_memory;
   return mem;
 }
 
 void deallocate(struct Memory* mem, struct Cell* cell) {
   cell->type = LISP_FREE;
+  cell->car.cell = mem->nil;
   cell->cdr = mem->free;
   mem->free = cell;
-}
-
-struct Cell* allocate(struct Memory* mem) {
-  struct Cell* new;
-  if (mem->nil == mem->free) {
-    printf("OUT OF MEMORY\n");
-    exit(1);
-  } else {
-    new = mem->free;
-    mem->free = mem->free->cdr;
-    return new;
-  }
 }
 
 void mark(struct Memory* mem, struct Cell* cell) {
@@ -87,11 +76,38 @@ void sweep(struct Memory* mem) {
   struct Cell* cur;
   for (i = 0; i < mem->size; ++i) {
     cur = &mem->all[i];
-    if (cur->mark) {
+    if (cur->mark == 1) {
       cur->mark = 0;
-    } else {
+    } else if (cur->type != LISP_FREE){
       deallocate(mem, cur);
     }
+  }
+}
+
+void run_collection(struct Memory* mem) {
+  mark(mem, mem->root);
+  sweep(mem);
+}
+
+struct Cell* _allocate(struct Memory* mem) {
+  struct Cell* new;
+  new = mem->free;
+  mem->free = mem->free->cdr;
+  new->cdr = mem->nil;
+  return new;
+}
+
+struct Cell* allocate(struct Memory* mem) {
+  if (mem->nil == mem->free) {
+    run_collection(mem);
+    if (mem->nil == mem->free) {
+      printf("OUT OF MEMORY\n");
+      exit(1);
+    } else {
+      return _allocate(mem);
+    }
+  } else {
+    return _allocate(mem);
   }
 }
 
@@ -125,26 +141,47 @@ struct Cell* new_cons(struct Memory* mem, struct Cell* a, struct Cell* b) {
 }
 
 ///// Testing/demo functions
+void print_cell_type(struct Cell* cell) {
+  switch (cell->type) {
+  case LISP_FREE: printf("."); break;
+  case LISP_INTEGER: printf("i"); break;
+  case LISP_CONS: printf("|"); break;
+  case LISP_CHARACTER: printf("c"); break;
+  default: printf("o");
+  }
+}
+
+void print_free_list(struct Memory* mem) {
+  struct Cell* cur = mem->free;
+  while (cur != mem->nil) {
+    print_cell_type(cur);
+    cur = cur->cdr;
+  }
+}
+
 void print_memory(struct Memory* mem) {
   int i;
   struct Cell* cur;
+  printf("      ");
   for (i = 0; i < mem->size; ++i) {
     cur = &mem->all[i];
-    switch (cur->type) {
-    case LISP_FREE: printf("."); break;
-    case LISP_INTEGER: printf("i"); break;
-    case LISP_CONS: printf("|"); break;
-    case LISP_CHARACTER: printf("c"); break;
-    default: printf("o");
-    }
-    if (cur->mark) printf("'");
+    if (cur->mark) printf("+");
     else printf(" ");
   }
-  printf("\n");
+  printf("\nMem:  ");
+  for (i = 0; i < mem->size; ++i) {
+    cur = &mem->all[i];
+    print_cell_type(cur);
+  }
+  printf("\nFree: ");
+  print_free_list(mem);
+  printf("\n\n");
 }
 
 void fill_memory(struct Memory* mem) {
+  printf("Filling: ");
   while (mem->nil != mem->free) {
+    print_cell_type(mem->free);
     new_integer(mem, 34);
   }
 }
@@ -154,7 +191,7 @@ void fill_memory(struct Memory* mem) {
 int main() {
   struct Cell* c;
   struct Cell* lst;
-  struct Memory* mem = new_memory(60);
+  struct Memory* mem = new_memory(100);
 
   print_memory(mem);
 
@@ -174,7 +211,7 @@ int main() {
   mark(mem, mem->root);
   print_memory(mem);
   sweep(mem);
-  print_memory(mem);  
+  print_memory(mem);
 
   lst = new_cons(mem, new_character(mem, 'a'), new_cons(mem, new_character(mem, 'b'), new_cons(mem, new_character(mem, 'c'), mem->nil)));
   mem->root = lst;
@@ -183,6 +220,8 @@ int main() {
   print_memory(mem);
   sweep(mem);
   print_memory(mem);
+  
+  fill_memory(mem);
 
   return 0;
 }
