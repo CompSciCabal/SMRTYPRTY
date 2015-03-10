56b0e4056d89b1496310b11c0261ef83cb793ebd
Chris Saunders <chris.saunders@shopify.com>
2015-03-10 12:38:06 +0000

    finished exercises for 1.3.1

--- sicp/v2/1.3/csaunders/exercises_1.3.1.rkt
+++ sicp/v2/1.3/csaunders/exercises_1.3.1.rkt
@@ -1,6 +1,11 @@
 #lang racket
 
 ;; Provided from SICP
+(define (gcd m n) 
+   (cond ((< m n) (gcd n m)) 
+         ((= n 0) m) 
+         (else (gcd n (remainder m n))))) 
+
 (define (sum term a next b)
   (if (> a b)
       0
@@ -183,7 +188,15 @@
                                     next
                                     b))))
 
-(displayln "exercise 1.33a.")
+(displayln "exercise 1.33 a.")
 (define (sum-of-square-primes a b)
   (filtered-accumulate + prime? 0 square a inc b))
 (sum-of-square-primes 1 5)
+
+(displayln "exercise 1.33 b.")
+(define (relative-prime? m n)
+  (= (gcd m n) 1))
+(define (product-of-relative-primes n)
+  (define (filter x) (relative-prime? x n))
+  (filtered-accumulate * filter 1 identity 1 inc n))
+(product-of-relative-primes 10)
