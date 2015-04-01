9eea56e88c2fbd70a042871ecc4172d896b4c4bf
timothy <t.williams.im@gmail.com>
2015-04-01 00:57:02 -0400

    1.3 sicp exercises

---
+++ sicp/v2/1.3/tmtwd/1.3_exercises.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+;1.31, how would I return 0 if the init value of a is greater 
+;than the init value of b?
+(define (product f a b)
+  (if (> a b)
+      1
+      (* (f a)
+         (product f
+                  ((lambda(x) (+ x 1)) a)
+                  b))))
