66033c33ef9d9e2bcf5c78e3457282897369a551
Chris Saunders <chris.saunders@shopify.com>
2015-04-30 13:59:18 +0000

    not quite done yet...

--- sicp/v2/2.5/csaunders/exercises.rkt
+++ sicp/v2/2.5/csaunders/exercises.rkt
@@ -1,6 +1,6 @@
 #lang racket
 ;; Prereqs
-(define dispatch-lookup '())
+(define dispatch-lookup (make-hash))
 (define (new-dispatch!)
 	(set! dispatch-lookup (make-hash)))
 
@@ -8,7 +8,9 @@
 	(hash-set! dispatch-lookup (cons type sig) proc))
 
 (define (get type sig)
-	(hash-ref dispatch-lookup (cons type sig)))
+  (if (hash-has-key? dispatch-lookup (cons type sig))
+      (hash-ref dispatch-lookup (cons type sig))
+      #f))
 
 (define (apply-generic op . args)
   (let* [(type-tags (map type-tag args))
@@ -102,3 +104,78 @@
   ;; Actual Homework 2.80
   (put '=zero? 'scheme-number (lambda (x) (= 0 x)))
   )
+
+(displayln "exercise 2.81")
+;; 2.81 a.
+;; there isn't an exp method defined, and because we have coercions that
+;; continually resolve (we added identity methods) we can continuously
+;; call apply-generic forever without ever reaching an end case.
+
+;; 2.81 b.
+;; No Louis is incorrect. Apply generic will work fine because if it doesn't
+;; find a method for data of the same type then it means that the method hasn't
+;; been defined
+
+;; 2.81 c.
+(define coercions (make-hash))
+(define (put-coercion from to proc)
+  (hash-set! coercions (cons from to) proc))
+(define (get-coercion from to)
+  (if (hash-has-key? (cons from to))
+      (hash-ref coercions (cons from to))
+      #f))
+(define (apply-generic-w-coercion op . args)
+  (let* [(type-tags (map type-tag args))
+         (proc (get op type-tags))]
+    (define (coercion-error)
+      (error "No method for these types" (list op type-tags)))
+    (if proc
+        (apply proc (map contents args))
+        (if (= (length args) 2)
+            (let* [(t1 (car type-tags))
+                   (t2 (cadr type-tags))
+                   (a1 (car args))
+                   (a2 (cadr args))
+                   (throw-err (if (eq? t1 t2) (coercion-error) '()))
+                   (t1->t2 (get-coercion t1 t2))
+                   (t2->t1 (get-coercion t2 t1))]
+              (cond [t1->t2 (apply-generic-w-coercion op (t1->t2 t1) t2)]
+                    [t2->t1 (apply-generic-w-coercion op t1 (t2->t1 t2))]
+                    [else (coercion-error)]))
+            (coercion-error)))))
+
+(displayln "exercise 2.82") ;; -- Still in progress
+;; Apply generic will call a function that coerces a list of items to the desired type
+(define (coerce-all-to desired others)
+  (define (any? test items)
+    (cond [(empty? items) #f]
+          [(test (car items)) #t]
+          [else (any? test (cdr items))]))
+  
+  (define (all? test items)
+    (cond [(empty? items) #t]
+          [(test (car items)) (all? test (cdr items))]
+          [else #f]))
+  
+  (let [(coercions (map (lambda (other) (get-coercion other desired)) others))]
+    (if (any? (lambda (x) (eq? #f x)) coercions)
+        #f
+        coercions)))
+
+(define (find-available-coercion types)
+  ;; This is a kinda shitty way of holding position in the list
+  ;; basically we are going to be moving a pointer through the list
+  ;; by simply keeping the head of the list in the types-in-front list
+  ;; so we basically end up with something like this:
+  ;; (a b c |d e f g) - | <- our current position (d is our current value)
+  ;;                         the items in before the | are the types-in-front
+  (define (coercions-iter position types-in-front)
+    (if (empty? position)
+        (error "No coercions could be found -- FIND-AVAILABLE-COERCION" types)
+    (let [(desired-type (car position))
+          (coerce-rest (coerce-all-to desired-type (cdr position)))
+          (coerce-front (coerce-all-to desired-type types-in-front))]
+          (if (and? coerce-rest coerce-front)
+              (append coerce-front '(no-coerce) coerce-rest)
+              (coercions-iter (cdr position) (append types-in-front (list (car position))))))))
+  (coercions-iter types '()))
