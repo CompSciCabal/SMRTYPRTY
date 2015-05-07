7ea688dd2a1035c18743c437a5f40aaef95c92ed
Chris Saunders <chris.saunders@shopify.com>
2015-05-07 12:56:42 +0000

    this is all I am going to finish for chapter 2

--- sicp/v2/2.5/csaunders/exercises.rkt
+++ sicp/v2/2.5/csaunders/exercises.rkt
@@ -273,6 +273,7 @@
 (define (negate x) (apply-generic 'negate x)) ;; exercise 2.88
 (define (add x y) (apply-generic 'add x y))
 (define (mul x y) (apply-generic 'mul x y))
+(define (div x y) (apply-generic 'div x y))
 (define (make-rational n d) (cons 'rational (cons n d)))
 (define (make-from-real-imag r i) (cons 'complex (cons r i)))
 
@@ -380,6 +381,26 @@
                       (mul (coeff t1) (coeff t2)))
            (mul-term-by-all-terms t1 (rest-terms L))))))
   
+  ;; Exercise 2.91
+  (define (div-terms L1 L2)
+    (if (empty-termlist? L1)
+        (list (the-empty-termlist) (the-empty-termlist))
+        (let ((t1 (first-term L1))
+              (t2 (first-term L2)))
+          (if (> (order t2) (order t1))
+              (list (the-empty-termlist) L1)
+              (let ((new-c (div (coeff t1) (coeff t2)))
+                    (new-o (- (order t1) (order t2))))
+                (let ((rest-of-result
+                       ;; Part we needed to add
+                       (div-terms (sub-terms L1
+                                             (mul-terms L2 (list (make-term new-o new-c)))))))
+                  (list (adjoin-term (make-term new-o new-c)
+                                     (car rest-of-result))
+                        (cadr (rest-of-result))))
+                       ;; End of part we needed to add
+                )))))
+  
   (define (add-poly p1 p2)
     (if (same-variable? (variable p1) (variable p2))
         (make-poly (variable p1)
@@ -396,12 +417,22 @@
         (error "Polys not in same var -- MUL-POLY"
                (list p1 p2))))
   
+  (define (div-poly p1 p2)
+    (if (same-variable (variable p1) (variable p2))
+        (make-poly (variable p1)
+                   (div-terms (term-list p1)
+                              (term-list p2)))
+        (error "Polys not in same var -- DIV-POLY"
+               (list p1 p2))))
+  
   ;; interface to rest of the system
   (define (tag p) (attach-tag 'polynomial p))
   (put 'add '(polynomial polynomial) 
        (lambda (p1 p2) (tag (add-poly p1 p2))))
   (put 'mul '(polynomial polynomial) 
        (lambda (p1 p2) (tag (mul-poly p1 p2))))
+  (put 'div '(polynomial polynomial)
+       (lambda (p1 p2) (tag (div-poly p1 p2))))
   (put 'make 'polynomial
        (lambda (var terms) (tag (make-poly var terms))))
   'done)
@@ -413,3 +444,6 @@
 ;; The interfaces I'm seeing are: adjoin-term, empty-termlist? make-term
 ;; If we ensure that a term 'object' needs to be a pair, we can reduce how much of an API needs to be rebuilt
 ;; for both of the new packages. The only tricky part would be selecting the correct term-list representation
+
+
+
