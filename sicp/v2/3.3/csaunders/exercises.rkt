7fc234488888b11eab31dfed7672f3b4e9d3ce66
Chris Saunders <chris.saunders@shopify.com>
2015-06-16 13:50:09 +0000

    Or gates, adders, etc.

--- sicp/v2/3.3/csaunders/exercises.rkt
+++ sicp/v2/3.3/csaunders/exercises.rkt
@@ -222,3 +222,94 @@
 ;; been memoized. So to first fill the table it will require doing a full
 ;; unmemoized fibonnaci function call, whereas making the calls in the memoized
 ;; version ensures that a function is never called more than once.
+
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+
+(define (logical-and a b)
+  (if (= a b 1) 1 0))
+
+(define (logical-or a b)
+  (cond [(= 1 a) 1]
+        [(= 1 b) 1]
+        [else 0]))
+        
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value
+           (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+(displayln "exercise 3.28")
+
+(define (or-gate o1 o2 output)
+  (define (and-action-procedure)
+    (let ([new-value (logical-or (get-signal o1) (get-signal o2))])
+      (after-delay or-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! o1 or-action-procedure)
+  (add-action! o2 or-action-procedure)
+  'ok)
+
+(displayln "exercise 3.29")
+
+#|
+|
+| NAND
+| IN1 -|
+| IN2 -| AND |- AndOut -| NOT |- OUT
+|-------------------------------------
+|  O1 -|    |
+|  O1 -|NAND|-- NandO1 -|    |
+|                       |NAND|-- Output
+|  O2 -|    |-- NandO2 -|    |
+|  O2 -|NAND|
+|-------------------------------------
+|
+| Delay time is that of 3 NAND gates. So that would be
+| 3x the delay for And + 3x the delay for NOT (inverter)
+|#
+(define (compound-or-gate o1 o2 output)
+  (define (nand-gate in1 in2 out)
+    (let [(andout (make-wire))]
+      (and-gate in1 in2 andout)
+      (inverter andout out)))
+    
+  (let ([nandO1 (make-wire)]
+        [nandO2 (make-wire)])
+    (nand-gate o1 o1 nandO1)
+    (nand-gate o2 o2 nandO2)
+    (nand-gate nandO1 nandO2 output)))
+
+(displayln "exercise 3.30")
+#|
+| The delay for a series of full adders will be that of
+| their parts. A full adder consists of: two half-adders
+| along with an OR gate.
+| The half adder is made up of two AND gates, an OR gate
+| along with an INVERTER.
+| So the propagation time for a single full adder would be:
+| (4*AND-DELAY + 3*OR-DELAY + 2*INVERTER-DELAY)
+| So a series consisting of N full adders would take:
+| N * (4*AND-DELAY + 3*OR-DELAY + 2*INVERTER-DELAY)
+|#
+    
+  
