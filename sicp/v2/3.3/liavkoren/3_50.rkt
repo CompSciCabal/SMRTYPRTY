d73811e4a07d61914a75a63babd4fdcd8f77ba8b
Liav Koren <liav@vmfarms.com>
2015-07-25 21:22:25 -0400

    Heavy magic.

--- sicp/v2/3.3/liavkoren/3_50.rkt
+++ sicp/v2/3.3/liavkoren/3_50.rkt
@@ -404,4 +404,60 @@ the power series for tangent.
 (define tan (div-series sine-series cosine-series))
 
 
+; ===================================================
+;
+; Section 3.5.3
+; -------------
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (sqrt-improve guess x)
+  (average guess (/ x guess)))
+
+(define (sqrt-stream x)
+  (define guesses
+    (cons-stream 1.0
+                 (stream-map (lambda (guess)
+                               (sqrt-improve guess x))
+                             guesses)))
+  guesses)
+
+; (display-stream (sqrt-stream 2))
+
+; Using Leibniz's formula for Pi, pi/4 = 1 - 1/3 + 1/5 - 1/7 + 1/9 - ...
+
+; The stream defined by (pi-summands 1) gives the sequence of fractions in the Leibniz formula.
+; Eg: (stream-ref (pi-summands 1) 0) = 1, (stream-ref (pi-summands 1) 1) = -1/3, etc...
+(define (pi-summands n)
+  (cons-stream (/ 1.0 n)
+               (stream-map - (pi-summands (+ n 2)))))
+
+; Remember, partial-sums takes a stream S and returns S0, S0 + S1, S0 + S1 + S2, ...
+(define pi-stream
+  (scale-stream (partial-sums (pi-summands 1)) 4))
+
+;(display-stream pi-stream)
+
+; Heavy magic follows
+(define (square x) (* x x))
+
+(define (euler-transform s)
+  (let ((s0 (stream-ref s 0))           ; Sn-1
+        (s1 (stream-ref s 1))           ; Sn
+        (s2 (stream-ref s 2)))          ; Sn+1
+    (cons-stream (- s2 (/ (square (- s2 s1))
+                          (+ s0 (* -2 s1) s2)))
+                 (euler-transform (stream-cdr s)))))
+
+(define (make-tableau transform s)
+  (cons-stream s
+               (make-tableau transform
+                             (transform s))))
+
+(define (accelerated-sequence transform s)
+  (stream-map stream-car
+              (make-tableau transform s)))
+
+(define pi (accelerated-sequence euler-transform pi-stream))
 
