cf6a6937c341c67c2a92b5bad5bee69cc29846dd
Liav Koren <liav@vmfarms.com>
2016-02-20 20:03:11 -0500

    Exercise 1.10

--- sicp/v3/1.2/liav_koren.rkt
+++ sicp/v3/1.2/liav_koren.rkt
@@ -49,4 +49,67 @@ because there is no information about the state of the computation stored in the
 |#
 
 
+#|
+
+Exercise 1.10
+=============
+The following procedure computes a mathematical function called Ackermann’s function.
+|#
+
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+
+; What are the values of the following expressions?
+
+
+(display "(A 1 10):")
+(A 1 10)
+(display "(A 2 4):")
+(A 2 4)
+(display "(A 3 3):")
+(A 3 3)
+
+
+#|
+; Consider the following procedures, where A is the procedure defined above:
+
+Give concise mathematical definitions for the functions computed by the procedures f, g,
+and h for positive integer values of n . For example, (k n) computes 5 n 2.|#
+
+
+(define (f n) (A 0 n))
+(f 0)
+(f 1)
+(f 2)
+(f 3)
+(f 4)
+(f 5)
+(f 10)
+(f 6.6)
+(f -5.5)
+; 2 * n
+(define (g n) (A 1 n))
+(g 0)
+(g 1)
+(g 2)
+(g 3)
+(g 4)
+(g 5)
+(g 10)
+; (g 6.6) does not terminate
+; (g -5) does note terminate
+; 2 ^ n
+
+(define (h n) (A 2 n))
+(h 1)
+(h 2)
+(h 3)
+(h 4)
+; negative and floats do not terminate
+; 2^2^2...n times, (Knuth up arrow).
+
 
