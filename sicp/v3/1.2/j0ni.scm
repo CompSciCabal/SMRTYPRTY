7ae4ed2513ba3dc3bc49d284be17addc06187d38
J Irving <j@lollyshouse.ca>
2016-02-28 19:31:54 -0500

    Ex 1.20

--- sicp/v3/1.2/j0ni.scm
+++ sicp/v3/1.2/j0ni.scm
@@ -538,3 +538,56 @@
                           p
                           q
                           (- count 1)))))
+
+;; Exercise 1.20: The process that a procedure generates is of course
+;; dependent on the rules used by the interpreter. As an example,
+;; consider the iterative gcd procedure given above. Suppose we were
+;; to interpret this procedure using normal-order evaluation, as
+;; discussed in 1.1.5. (The normal-order-evaluation rule for if is
+;; described in Exercise 1.5.) Using the substitution method (for
+;; normal order), illustrate the process generated in evaluating (gcd
+;; 206 40) and indicate the remainder operations that are actually
+;; performed. How many remainder operations are actually performed in
+;; the normal-order evaluation of (gcd 206 40)? In the
+;; applicative-order evaluation?
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+;; Normal order:
+
+;; (gcd 206
+;;      40)
+
+;; (gcd 40
+;;      (remainder 206 40))
+
+;; (gcd (remainder 206 40)
+;;      (remainder 40 (remainder 206 40)))
+
+;; (gcd (remainder 40 (remainder 206 40))
+;;      (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))
+
+;; (gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+;;      (remainder (remainder 40 (remainder 206 40))
+;;                 (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))
+
+;; so 14 evaluations of `remainder` in the conditional and 4
+;; evaluations for returning a, which is value 2. 18 in total.
+
+;; Applicative order:
+
+;; (gcd 206 40)
+;; (gcd 40 (remainder 206 40))
+;; (gcd 40 6)
+;; (gcd 6 (remainder 40 6))
+;; (gcd 6 4)
+;; (gcd 4 (remainder 6 4))
+;; (gcd 4 2)
+;; (gcd 2 (remainder 4 2))
+;; (gcd 2 0)
+;; 2
+
+;; 4 evaluations over all
