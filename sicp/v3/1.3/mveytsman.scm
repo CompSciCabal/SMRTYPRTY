705bbf86e18febdbb8bbe03f824c04ac781b202e
Max Veytsman <maxim@ontoillogical.com>
2016-04-03 12:18:26 -0400

    had arguments in wrong order for combiner

--- sicp/v3/1.3/mveytsman.scm
+++ sicp/v3/1.3/mveytsman.scm
@@ -145,7 +145,7 @@
 (define (accumulate combiner null-value term a next b)
   (if (> a b)
       null-value
-      (combiner (term a) (accumulate combiner null-value term (next a) next b))))
+      (combiner (accumulate combiner null-value term (next a) next b) (term a) )))
 
 (define (sum term a next b)
   (accumulate + 0 term a next b))
@@ -161,7 +161,7 @@
   (define (iter a result)
     (if (> a b)
         result
-        (iter (next a) (combiner (term a) result))))
+        (iter (next a) (combiner result (term a)))))
   (iter a null-value))
 
 
