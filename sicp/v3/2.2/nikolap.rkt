35a184a3803e8202485c8996cfe0efb5ce02b3b8
Nikola <nikola.peric03@gmail.com>
2016-04-10 11:39:32 -0400

    Section 2.2.2

--- sicp/v3/2.2/nikolap.rkt
+++ sicp/v3/2.2/nikolap.rkt
@@ -76,19 +76,132 @@
  true)
 
 ;; 2.24
+;; '(1 '(2 '(3 4)))
+;; Let's forgo the diagrams on this document :)
 
 ;; 2.25
+(cadr ;; 7
+  (cadr ;; (5 7)
+   (cdr ;; 3 (5 7)..
+           '(1 3 '(5 7) 9))))
+
+(cadr (cadr (cadr (cadr (cadr (cadr '(1 '(2 '(3 '(4 '(5 '(6 '(7)))))))))))) ;; 6x cadr
 
 ;; 2.26
+;; (append x y) => '(1 2 3 4 5 6)
+;; (cons x y) => '(1 2 3 '(4 5 6))
+;; (list x y) => '('(1 2 3) '(4 5 6))
 
 ;; 2.27
+;; Abstraction for 2.27, 2.28, 2.30
+(define (tree-recursion coll if-empty if-pair if-else)
+ (cond ((null? coll) if-empty)
+  ((pair? coll) (if-pair coll))
+  (else (if-else coll))))
+
+(define (deep-reverse coll)
+ (tree-recursion
+   coll
+   nil
+   (lambda (l) (append (deep-reverse (cdr l))
+                       (list (deep-reverse (car l)))))
+   (lambda (l) (append (deep-reverse (cdr l)) (list (car l))))))
 
 ;; 2.28
+(define (fringe coll)
+ (tree-recursion
+   coll
+   nil
+   (lambda (l) (append (fringe (car coll)) (fringe (cdr coll))))
+   (lambda (l) (list coll))))
 
 ;; 2.29
+; a)
+(define (left-branch mobile)
+ (car mobile))
+
+(define (right-branch mobile)
+ (cadr mobile))
+
+(define (branch-length branch)
+ (car branch))
+
+(define (branch-structure branch)
+ (cadr branch))
+
+ ; b)
+(define (branch-weight branch)
+ (if (pair? (branch-structure branch))
+  (total-weight (branch-structure branch))
+  (branch-structure branch)))
+
+(define (total-weight mobile)
+ (+ (branch-weight (left-branch mobile))
+  (branch-weight (right-branch mobile))))
+
+; c)
+(define (branch-torque branch)
+ (* (branch-length branch)
+  (branch-weight branch)))
+
+(define (branch-balanced? branch)
+ (or (not (pair? (branch-structure branch)))
+  (mobile-balanced? (branch-structure branch))))
+
+(define (mobile-balanced? mobile)
+ (and (= (branch-torque (left-branch mobile))
+         (branch-torque (right-branch mobile)))
+      (branch-balanced? (left-branch mobile))
+      (branch-balanced? (right-branch mobile))))
+
+; d)
+;; only the selectors (left-branch, right-branch, branch-length, branch-structure) need to be changed
 
 ;; 2.30
+(define (square-tree-direct tree)
+ (tree-recursion
+   tree
+   nil
+   (lambda (t)
+    (cons (square-tree-direct (car tree))
+          (square-tree-direct (cdr tree))))
+   (lambda (t) (* t t))))
+
+(define (square-tree-map tree)
+ (map (lambda (t)
+       (if (pair? t)
+        (square-tree-map t)
+        (* t t)))
+      tree))
 
 ;; 2.31
+;; whoohoo better abstraction than my earlier tree-recursion fn
+(define (tree-map proc tree)
+ (cond ((null? tree) null)
+       ((pair? tree) (cons (tree-map proc (car tree))
+                           (tree-map proc (cdr tree))))
+       (else (proc tree)))
 
 ;; 2.32
+(define (subsets s)
+ (if (null s)
+  (list nil) ;; if s is nil, then subsets are an empty list
+  (let ((rest (subsets (cdr s))))
+   (append rest (map
+                 (lambda (r)
+                  (cons (car s) r))
+                 rest)))))
+;; let's walk through an example
+;; (subset (1 2 3))
+;; rest => (subset (2 3))
+;;          rest => (subset (3))
+;;                rest => ()
+;; the appending is to append the last empty rest to the mapped coll (an empty set is still a set!)
+;;                    (cons 2 nil) => 2
+;;                (cons 3 nil) => 3
+;;         (cons 2 3) => 2 3
+;;    (cons 1 nil) => 1
+;;    (cons 1 3) => 1 3
+;;    (cons 1 2) => 1 2
+;;    (cons (1 2) 3) => 1 2 3
+;; then just append the empty set and voila
