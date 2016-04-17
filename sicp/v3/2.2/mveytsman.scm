786dc841d70bc4d74764a6c6a90da8cbb6a2ebbe
Max Veytsman <maxim@ontoillogical.com>
2016-04-17 14:23:01 -0400

    Finished 2.2.2

--- sicp/v3/2.2/mveytsman.scm
+++ sicp/v3/2.2/mveytsman.scm
@@ -211,8 +211,8 @@
   (if (null? lst)
       #t
       (begin
-          (f (car lst))
-          (for-each f (cdr lst)))))
+        (f (car lst))
+        (for-each f (cdr lst)))))
 
 ;; I cheated above and used begin so we can handle the empty list case, even though I don't think we hit begin in the book yet.
 
@@ -224,3 +224,238 @@
   (if (null? (cdr lst))
       #t
       (for-each f (cdr lst))))
+
+;; -------------------------------------------------------
+
+;; Exercise 2.24: Suppose we evaluate the expression (list 1 (list 2 (list 3
+;; 4))). Give the result printed by the interpreter, the corresponding
+;; box-and-pointer structure, and the interpretation of this as a tree (as in
+;; Figure 2.6).
+
+;; The interpreter gives (1 (2 (3 4)))
+
+;; (1 (2 (3 4)  (2 (3 4) (3 4)
+;;      +        +        +
+;;      |        |        |
+;;      |        |        |
+;;      +---+    +---+    +---+    +---+
+;;      |.|.+--->+.|.+--->+.|.+--->+.|/|
+;;      ++--+    ++--+    ++--+    ++--+
+;;      |        |        |        |
+;;      v        v        v        v
+;;      +++      +++      +++      +++
+;;      |1|      |2|      |3|      |4|
+;;      +-+      +-+      +-+      +-+
+
+
+;;; -----------------------------
+
+;; Exercise 2.25: Give combinations of cars and cdrs that will pick 7 from each of the following lists:
+
+
+(car (cdr (car (cdr (cdr '(1 3 (5 7) 9))))))
+;; => 7
+
+(car (car '((7))))
+;; => 7
+
+(car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr '(1 (2 (3 (4 (5 (6 7))))))))))))))))))
+;; => 7
+
+;; -----------------------------------
+
+;; Exercise 2.26: Suppose we define x and y to be two lists:
+
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+;; What result is printed by the interpreter in response to evaluating each of the following expressions:
+
+(append x y)
+;; => (1 2 3 4 5 6)
+
+(cons x y)
+;; => ((1 2 3) 4 5 6)
+
+(list x y)
+;; => ((1 2 3) (4 5 6))
+
+;; Exercise 2.27: Modify your reverse procedure of Exercise 2.18 to produce a
+;; deep-reverse procedure that takes a list as argument and returns as its value
+;; the list with its elements reversed and with all sublists deep-reversed as
+;; well. For example,
+
+
+(define (deep-reverse lst)
+  (define (iter a b)
+    (if (null? b)
+        a
+        (iter (cons (deep-reverse (car b)) a)(cdr b))))
+  (if (pair? lst)
+      (iter nil lst)
+      lst))
+
+
+(define x 
+  (list (list 1 2) (list 3 4)))
+
+x
+;; => ((1 2) (3 4))
+
+(reverse x)
+;; => ((3 4) (1 2))
+
+(deep-reverse x)
+;; => ((4 3) (2 1))
+
+;; Exercise 2.28: Write a procedure fringe that takes as argument a tree
+;; (represented as a list) and returns a list whose elements are all the leaves
+;; of the tree arranged in left-to-right order. For example,
+
+(define (fringe tree)
+  (if (pair? tree)
+      (if (null? (cdr tree))
+          (fringe (car tree))
+          (append (fringe (car tree)) (fringe (cdr tree))))
+      (list tree)))
+
+(define x 
+  (list (list 1 2) (list 3 4)))
+
+(fringe x)
+(1 2 3 4)
+
+(fringe (list x x))
+(1 2 3 4 1 2 3 4)
+
+;; ---------------------------------
+
+;; Exercise 2.29: A binary mobile consists of two branches, a left branch and a
+;; right branch. Each branch is a rod of a certain length, from which hangs
+;; either a weight or another binary mobile. We can represent a binary mobile
+;; using compound data by constructing it from two branches (for example, using
+;; list):
+
+(define (make-mobile left right)
+  (list left right))
+
+;; A branch is constructed from a length (which must be a number) together with a structure, which may be either a number (representing a simple weight) or another mobile:
+
+(define (make-branch length structure)
+  (list length structure))
+
+;; 1. Write the corresponding selectors left-branch and right-branch, which return
+;; the branches of a mobile, and branch-length and branch-structure, which
+;; return the components of a branch.
+
+(define left-branch car))
+
+(define right-branch cadr)
+
+(define branch-length car)
+
+(define branch-structure cadr)
+;; 2. Using your selectors, define a procedure total-weight that returns the
+;; total weight of a mobile.
+
+(define mobile? pair?)
+
+(define (branch-weight branch)
+  (if (mobile? (branch-structure branch))
+      (total-weight (branch-structure branch))
+      (branch-structure branch)))
+(define (total-weight mobile)
+  (+ (branch-weight (left-branch mobile)) (branch-weight (right-branch mobile))))
+
+;; 3. A mobile is said to be balanced if the torque applied by its top-left
+;; branch is equal to that applied by its top-right branch (that is, if the
+;; length of the left rod multiplied by the weight hanging from that rod is
+;; equal to the corresponding product for the right side) and if each of the
+;; submobiles hanging off its branches is balanced. Design a predicate that
+;; tests whether a binary mobile is balanced.
+
+(define (balanced? mobile)
+  (define (torque branch)
+    (* (branch-length branch)
+       (branch-weight branch)))
+  (if (mobile? mobile)
+      (and
+       (= (torque (left-branch mobile))
+          (torque (right-branch mobile)))
+       (balanced? (left-branch mobile))
+       (balanced (right-branch mobile)))
+      #t))
+
+
+;; 4. Suppose we change the representation of mobiles so that the constructors are
+;; (define (make-mobile left right)
+;;   (cons left right))
+
+;; (define (make-branch length structure)
+;;   (cons length structure))
+;; How much do you need to change your programs to convert to the new representation?
+
+;; We'd have to rewrite right-branch and branch-structure to be cdr instead of cadr. Everything will then work.
+
+;; --------------------------------
+
+;; Exercise 2.30: Define a procedure square-tree analogous to the square-list procedure of Exercise 2.21. That is, square-tree should behave as follows:
+
+(square-tree
+ (list 1
+       (list 2 (list 3 4) 5)
+       (list 6 7)))
+;; => (1 (4 (9 16) 25) (36 49))
+
+;;Define square-tree both directly (i.e., without using any higher-order procedures) and also by using map and recursion.
+
+(define (square-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree))
+         (square tree))
+        (else
+         (cons (square-tree (car tree))
+               (square-tree (cdr tree))))))
+
+
+(define (square-tree tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (square-tree sub-tree)
+             (square sub-tree)))
+       tree))
+
+;; -------------------------------------------------
+;; Exercise 2.31: Abstract your answer to Exercise 2.30 to produce a procedure
+;; tree-map with the property that square-tree could be defined as
+
+(define (square-tree tree)
+  (tree-map square tree))
+
+(define (tree-map f tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (tree-map f sub-tree)
+             (f sub-tree)))
+       tree))
+
+;; ------------------------------------------------------
+
+;; Exercise 2.32: We can represent a set as a list of distinct elements, and we
+;; can represent the set of all subsets of the set as a list of lists. For
+;; example, if the set is (1 2 3), then the set of all subsets is (() (3) (2) (2
+;; 3) (1) (1 3) (1 2) (1 2 3)). Complete the following definition of a procedure
+;; that generates the set of subsets of a set and give a clear explanation of
+;; why it works:
+
+(define (subsets s)
+  (if (null? s)
+      (list nil)
+      (let ((rest (subsets (cdr s))))
+        (append rest (map (lambda (x)
+                            (cons (car s) x)) rest)))))
+
+
+;; It works by induction. The subsets of an empty set is empty. If the S' is the
+;; set S with the element x added, then the subsets of S' are all the subsets of
+;; S, along with each of those subsets with x included in them.
