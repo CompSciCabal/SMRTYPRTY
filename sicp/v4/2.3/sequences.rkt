01df64dded9dfb47e948d60e344334b260eed056
Chun Li <chun@bibliocommons.com>
2017-08-02 19:42:05 -0400

    Finished sequences for 2.3

--- sicp/v4/2.3/sequences.rkt
+++ sicp/v4/2.3/sequences.rkt
@@ -4,25 +4,25 @@
   (cond ((null? sequence) null)
         ((predicate (car sequence))
          (cons (car sequence)
-               (filter predicate 
+               (filter predicate
                        (cdr sequence))))
-        (else  (filter predicate 
+        (else  (filter predicate
                        (cdr sequence)))))
 
 (define (accumulate op initial sequence)
   (if (null? sequence)
       initial
       (op (car sequence)
-          (accumulate op 
-                      initial 
+          (accumulate op
+                      initial
                       (cdr sequence)))))
 
 (define (enumerate-interval low high)
   (if (> low high)
       null
-      (cons low 
+      (cons low
             (enumerate-interval
-              (+ low 1) 
+              (+ low 1)
               high))))
 
 ;; 2.33
@@ -30,11 +30,11 @@
 ;; Uncomment if you want to test below.
 #| (define (map p sequence) |#
 #|   (accumulate (lambda (x y) |#
-#|                 (cons (p x) y)) |# 
+#|                 (cons (p x) y)) |#
 #|               null sequence)) |#
-
-(define (append seq1 seq2)
-  (accumulate cons seq2 seq1))
+;; Later we need to use a different append.
+#| (define (append seq1 seq2) |#
+#|   (accumulate cons seq2 seq1)) |#
 
 (define (length sequence)
   (accumulate (lambda (el counter) (+ counter 1)) 0 sequence))
@@ -120,19 +120,65 @@
 (define (matrix-*-matrix m n)
   (let ((cols (transpose n)))
     (map (lambda (row)
-           (map (lambda (col) 
+           (map (lambda (col)
                   (dot-product row col))
                 cols)) m)))
 
-(define m '((1 2 3 4) (4 5 6 6) (6 7 8 9)))
-(define i '((1 0 0 0) (0 1 0 0) (0 0 1 0) (0 0 0 1)))
-(define v '(2 1 10 20))
-(define r1 '(1 2 3 4))
-(define r2 '(4 5 6 6))
-(define r3 '(6 7 8 9))
-(dot-product v r1) ;; should be 2+2+30+80 = 114
-(dot-product v r2) ;; should be 193
-(dot-product v r3) ;; should be 279
-(matrix-*-vector m v) ;; should be '(114 193 279)
-(transpose m) ;; should be '((1 4 6) (2 5 7) (3 6 8) (4 6 9))
-(matrix-*-matrix m i) ;; should be m
+#| (define m '((1 2 3 4) (4 5 6 6) (6 7 8 9))) |#
+#| (define i '((1 0 0 0) (0 1 0 0) (0 0 1 0) (0 0 0 1))) |#
+#| (define v '(2 1 10 20)) |#
+#| (define r1 '(1 2 3 4)) |#
+#| (define r2 '(4 5 6 6)) |#
+#| (define r3 '(6 7 8 9)) |#
+#| (dot-product v r1) ;; should be 2+2+30+80 = 114 |#
+#| (dot-product v r2) ;; should be 193 |#
+#| (dot-product v r3) ;; should be 279 |#
+#| (matrix-*-vector m v) ;; should be '(114 193 279) |#
+#| (transpose m) ;; should be '((1 4 6) (2 5 7) (3 6 8) (4 6 9)) |#
+#| (matrix-*-matrix m i) ;; should be m |#
+
+
+;; 2.38
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op
+                      initial
+                      (cdr sequence)))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+;; the operation needs to be associative (grouping does not matter)
+;; Note that I did not say commutative; matrix multiplication for example will work
+;; fine with either fold-right or fold-left.
+
+#| (fold-right / 1 (list 1 2 3)) ; should give 1/(2/3) = 3/2 |#
+#| (fold-left  / 1 (list 1 2 3)) ; should give (1/2)/3 = 1/6 |#
+#| (fold-right list empty (list 1 2 3)) ; should be (list 1 (list 2 (list 3 (list empty)))) |#
+#|                                      ; or '(1 (2 (3 ()))) |#
+#| (fold-left  list empty (list 1 2 3)) ; (list (list (list (list empty) 1) 2) 3) |#
+#|                                      ; or '(((() 1) 2) (3)) |#
+
+
+;; 2.39
+; redefine append as before so second arg is an element
+(define (append lst el)
+  (cond [(empty? lst) (cons el empty)]
+        [else (cons (car lst) 
+                    (append (cdr lst) el))]))
+(define (reverse sequence)
+  (fold-right 
+   (lambda (x y) (append y x)) null sequence))
+
+#| (define (reverse sequence) |#
+#|   (fold-left |# 
+#|    (lambda (x y) (cons y x)) null sequence)) |#
+
+(reverse (list 1 2 3 4))
