d8652d4b68b21e77d643f59da8b31519846140a2
Pete Bevin <pete@petebevin.com>
2019-06-20 13:27:55 -0400

    Simple graph search

--- paip/pete/ch6/Search.hs
+++ paip/pete/ch6/Search.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Search where
 
-import Data.List
-import Data.Function
+import           Data.Function
+import           Data.List
+import           Data.Set      (Set)
+import qualified Data.Set      as Set
 
-import Debug.Trace
+import           Debug.Trace
 
 treeSearch :: Show a => [a] -> (a -> Bool) -> (a -> [a]) -> ([a] -> [a] -> [a]) -> Maybe a
 treeSearch startStates isGoal successors combine =
@@ -14,13 +18,36 @@ treeSearch startStates isGoal successors combine =
           [] -> Nothing
           x:xs -> if isGoal x then Just x else search (combine (successors x) xs)
 
+-- graphSearch startStates isGoal successors combine
+-- attempts to find a state that satisfies isGoal,
+-- starting with startStates, and searching according
+-- to successors and combine.  It does not try the same
+-- state twice.
+graphSearch :: (Ord a, Show a) => [a] -> (a -> Bool) -> (a -> [a]) -> ([a] -> [a] -> [a]) -> Maybe a
+graphSearch startStates isGoal successors combine =
+  search startStates (Set.fromList startStates)
+    where
+      search states seen =
+        case traceShowId states of
+          [] ->
+            Nothing
+          x:xs | isGoal x ->
+            Just x
+          x:xs | otherwise ->
+            let stepStates = filter (\y -> Set.notMember y seen') (successors x)
+                seen' = Set.insert x seen
+                states' = nub (combine (successors x) xs)
+             in search states' seen'
+
 depthFirstSearch :: Show a => a -> (a -> Bool) -> (a -> [a]) -> Maybe a
 depthFirstSearch start isGoal successors =
   treeSearch [start] isGoal successors (++)
 
 breadthFirstSearch :: Show a => a -> (a -> Bool) -> (a -> [a]) -> Maybe a
 breadthFirstSearch start isGoal successors =
-  treeSearch [start] isGoal successors (flip (++))
+  treeSearch [start] isGoal successors prepend
+
+prepend = flip (++)
 
 bestFirstSearch :: (Show a, Ord b) => a -> (a -> Bool) -> (a -> [a]) -> (a -> b) -> Maybe a
 bestFirstSearch start isGoal successors cost =
@@ -53,3 +80,5 @@ binaryTree n = [2*n, 2*n+1]
 finiteBinaryTree :: Integer -> Integer -> [Integer]
 finiteBinaryTree sup n = filter (< sup) (binaryTree n)
 
+next2 :: Integer -> [Integer]
+next2 n = [n+1, n+2]
